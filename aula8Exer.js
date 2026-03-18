c4b4b006acfd8745aff77adf6f4f217db5807fbc
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-03-18 12:29:17 -0300

    Add files via upload

---
+++ aula8Exer.js
@@ -0,0 +1,15 @@
+const Modelo = [
+    {
+        Obra : "O Iluminado",
+        dataLanç : "28/01/1977",
+        Autor : "Stephen King",
+        Páginas : "440 e 584",
+        Sinopse : "O Iluminado, de Stephen King, narra o isolamento da família Torrance no Hotel Overlook, no Colorado, durante o inverno. Jack, um escritor alcoólatra, busca recomeçar, mas o hotel sinistro atiça seus demônios internos e persegue seu filho, Danny, que possui poderes psíquicos (iluminado), transformando o local em uma armadilha mortal.",
+        Preço : "de 80,00 a 149,00"
+    }
+]
+ 
+const Livro = [
+
+    
+]
