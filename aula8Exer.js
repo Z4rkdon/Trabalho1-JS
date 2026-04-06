21f0f48899188b658c49fadf07266b6cd49e460f
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-04-06 09:40:05 -0300

    Update aula8Exer.js

--- aula8Exer.js
+++ aula8Exer.js
@@ -81,8 +81,7 @@ const Serie = [
     },
     
 ]
- 
-const SerieJSON = JSON.stringify(Serie)
+ const SerieJSON = JSON.stringify(Serie)
 console.log(Serie)
 console.log(SerieJSON)
 console.log([0].Serie)
@@ -102,31 +101,3 @@ console.log("-------------------------------------------------------")
 criarArquivo(Serie);
 const arquivo = require("./Serie.json")
 console.log(arquivo)
-
-
-//criarArquivo(livros)
-let arquivo
-function lerArquivo(){
-    arquivo = require("./Serie2.json")
-}
-lerArquivo()
-
-function mostrarLivros(lista){
-    console.log("##### ESTOQUE DE SERIES #####")
-    lista.forEach((Serie) => {
-        console.log(Serie.titulo + " Apenas um Show " + 
-                    Serie.autor + " Ano:24/08/2015 " + 
-                    Serie.ano + " Páginas:" +
-                    Serie.paginas + " Preço: R$ "+
-                    Serie.preco.toFixed(2).replaceAll(".",","))
-    })
-}
-mostrarLivros(arquivo)
-
-let livrosFiltrados = arquivo.filter((livro) => livro.preco > 100)
-mostrarLivros(livrosFiltrados)
-
-let livrosDesconto = arquivo.map((livro) => {
-    return {...livro, preco: livro.preco * 0.9}
-})
-mostrarLivros(livrosDesconto)
