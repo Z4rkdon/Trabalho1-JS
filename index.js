ec689036956a76c91a09ca0f268a909b0fece800
Z4rkdon <goveia.silva.guilherme@escola.pr.gov.br>
2026-05-27 08:58:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ const port = 3000
 app.use(express.json())
 const fs = require('fs')
 
-app.get('/ola', (req, res)=>{
-    res.json("Hello Class!")
+app.post("/clientes/cadastro", (req,res) => {
+    const cliente = req.body
+    if (!cliente || Object.keys(cliente).length === 0) {
+        res.status(400).json({resposta: "Body não preenchido"})
+    } else {
+        try {
+            const bd = JSON.parse(fs.readFileSync('bd.json', 'utf8'))
+            bd.push(cliente)
+            fs.writeFileSync('bd.json', JSON.stringify(bd), 'utf8')
+            res.status(200).json({resposta: "Cliente cadastrado com sucesso!"})
+        } catch(error) {
+            res.status(500).json({resposta: error.message})
+        }
+    }    
 })
 
-app.post('/clientes/cadatro', (req, res)=>{
-    const dados = req.body
-    const bd = fs.readFileSync("bd.json", "utf8")
-    const bdOBJ = JSON.parse(bd)
-    bd.push(dados)
-    fs.writeFileSync()
-    console.log(bdOBJ)
-    res.json({resposta : ok})
-})
 
 app.listen(port, ()=>{
     console.log("API executando na porta" + port)
